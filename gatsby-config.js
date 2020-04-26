27b89376956d6291e14f3c8b1f5afb115cef8ffd
Luiz Felipe Gonçalves <23662020+lffg@users.noreply.github.com>
2020-04-26 12:34:16 -0300

    chore: Add layouts component

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,18 +3,28 @@ module.exports = {
     title: 'Luiz Felipe Gonçalves',
     description: "Luiz Felipe Gonçalves' website.",
     author: 'Luiz Felipe Gonçalves',
+
     social: {
       github: 'lffg',
       twitter: '_lffg',
       telegram: 'luizffg',
       discord: 'Luiz#2029'
-    },
-    defaultLang: 'en'
+    }
   },
 
   plugins: [
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-typescript',
-    'gatsby-plugin-sass'
+    'gatsby-plugin-sass',
+    {
+      resolve: 'gatsby-plugin-google-fonts',
+      options: {
+        fonts: [
+          'Merriweather:ital,wght@0,400;0,700;1,400;1,700',
+          'Montserrat:ital,wght@0,800;0,900;1,800'
+        ],
+        display: 'swap'
+      }
+    }
   ]
 };
