eb21b7a3d8fa1b1fedbd3704c247fd1b02c0c3aa
Luiz Felipe Gonçalves <23662020+lffg@users.noreply.github.com>
2020-05-15 21:46:27 -0300

    feat: Add `getTranslations`

--- config/i18n.ts
+++ config/i18n.ts
@@ -1,4 +1,6 @@
-export const config = {
+import type { PromiseValue } from 'type-fest';
+
+export const i18nOptions = {
   en: {
     default: true,
     ogLanguage: 'en_US'
@@ -8,3 +10,34 @@ export const config = {
     ogLanguage: 'pt_BR'
   }
 };
+
+export function getTranslations() {
+  return getKeys({
+    pt: import('./locales/pt'),
+    en: import('./locales/en')
+  });
+}
+
+// PRIVATE HELPERS
+// ===============
+
+async function getKeys<
+  T extends {
+    [key: string]: Promise<{ translations: unknown }>;
+  }
+>(
+  translations: T
+): Promise<{ [key in keyof T]: PromiseValue<T[key]>['translations'] }> {
+  const final: any = {};
+
+  for (const [key, translation] of await Promise.all(
+    Object.entries(translations).map(
+      async ([key, value]) =>
+        [key, (await value).translations] as [string, unknown]
+    )
+  )) {
+    final[key] = translation;
+  }
+
+  return final;
+}
