abdd6f415b31cd68f1ebfca51fa97b8e9318292c
Luiz Felipe Gonçalves <23662020+lffg@users.noreply.github.com>
2020-06-01 16:21:39 -0300

    feat: Add `ensureSlashes`

--- src/utils/slashes.ts
+++ src/utils/slashes.ts
@@ -1,3 +1,21 @@
 export function trimSlashes(str: string) {
   return str.replace(/^\/+|\/+$/g, '');
 }
+
+export interface EnsureSlashesOptions {
+  start?: boolean;
+  end?: boolean;
+}
+
+export function ensureSlashes(str: string, options: EnsureSlashesOptions = {}) {
+  const mergedOptions: EnsureSlashesOptions = {
+    start: true,
+    end: true,
+    ...options
+  };
+
+  const start = mergedOptions.start ? '/' : '';
+  const end = mergedOptions.end ? '/' : '';
+
+  return start + trimSlashes(str) + end;
+}
