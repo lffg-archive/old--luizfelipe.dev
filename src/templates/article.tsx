94e69e03e4f8d00b08363d49c5b0ba7a57895805
Luiz Felipe Gonçalves <23662020+lffg@users.noreply.github.com>
2020-06-13 01:11:05 -0300

    fix: Remove unused code

--- src/templates/article.tsx
+++ src/templates/article.tsx
@@ -1,21 +1,16 @@
 import { graphql } from 'gatsby';
 import { MDXRenderer } from 'gatsby-plugin-mdx';
 import React from 'react';
-import { LocalizedLink } from '../components/localized-link';
 import { SEO } from '../components/seo';
-import { useTranslations } from '../context/translations';
 import type { PageProps } from '../types/gatsby';
 
 export default function Article({ data }: PageProps<Data>) {
-  const { site } = useTranslations();
-
   const { body, frontmatter } = data.article;
 
   return (
     <>
       <SEO title={frontmatter.title} />
 
-      <LocalizedLink to="/">{site.home}</LocalizedLink>
       <h1>{frontmatter.title}</h1>
       <MDXRenderer>{body}</MDXRenderer>
     </>
